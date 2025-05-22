2427fce7f3e2ba8ac8720ca7d652b0d82f35bbac
alvarofernand0 <alvarofernando1324@gmail.com>
2025-05-22 15:48:28 -0300

    Primeiro commit

---
+++ pratica-class/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+var pessoa = new Pessoa()
+{
+    Nome = "Antonio Nunes",
+    Idade = 26,
+    Endereco = "Rua Jose de Alfredo, 465, Masp, Curitiba, PR"
+};
+    Console.WriteLine($"Nome da pessoa: {pessoa.Nome}");
+    Console.WriteLine($"Endereço: {pessoa.Endereco}");
+
+
+
---
+++ pratica-operacoes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um número: ");
+        string opcao = Console.ReadLine();
+        if (int.TryParse(opcao, out int num))
+        {
+            
+        }
+        
+       
+
+
+
+
+    }
+}
