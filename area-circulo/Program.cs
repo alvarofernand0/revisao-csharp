870fda4981cb87b3fbb086ed7b3fdb5a50a209a2
alvarofernand0 <alvarofernando1324@gmail.com>
2025-05-26 14:02:07 -0300

    ajustando de novo gitignore

---
+++ area-circulo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double raio1 = double.Parse(Console.ReadLine());
+        double n = 3.14159;
+
+        double raio = raio1 * raio1;
+
+        double area = raio * n;
+        Console.WriteLine($"A={area:f4}");
+    }
+}
