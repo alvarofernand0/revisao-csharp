d6f02082a24227095fc3d81907bd5b371dbc8984
alvarofernand0 <alvarofernando1324@gmail.com>
2025-05-26 12:47:42 -0300

    Adicionando soma-simples

---
+++ soma-simples/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o 1° Número: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o 2°: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int X = a + b;
+
+        Console.WriteLine($"X = {X}");
+
+    }
+}
