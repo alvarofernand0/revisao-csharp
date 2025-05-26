15ab9cabd3067c8936a90a50b8962e0f069094bd
alvarofernand0 <alvarofernando1324@gmail.com>
2025-05-26 14:21:04 -0300

    Adicionando produto-simples

--- produto-simples/Program.cs
+++ produto-simples/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Net.Http.Headers;
+
+class Program
+{
+    static void Main()
+    {
+        int var1 = int.Parse(Console.ReadLine());
+        int var2 = int.Parse(Console.ReadLine());
+
+        int prod = var1 * var2;
+
+        Console.WriteLine($"PROD = {prod}");
+    }
+
+}
