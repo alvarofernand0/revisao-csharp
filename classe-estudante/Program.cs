cb1ec5d31741fb835eb201b2e2a10cb907c9c770
alvarofernand0 <alvarofernando1324@gmail.com>
2025-05-29 10:53:11 -0300

    Corrigindo classe-estudante

--- classe-estudante/Program.cs
+++ classe-estudante/Program.cs
@@ -5,12 +5,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        string nome = Console.ReadLine();
-        string matricula = Console.ReadLine();
-        double notas = double.Parse(Console.ReadLine());
+        // string nome = "Antonio"; //Console.ReadLine();
+        // string matricula = "784934938"; //Console.ReadLine();
+        // double notas = 8.5; //double.Parse(Console.ReadLine());
 
-        Estudante estd = new Estudante(nome, matricula);
-        estd.AdicionarNota(notas);
+        Estudante estd = new Estudante("Antonio", "784934938");
+        estd.AdicionarNota(8.5);
+        estd.AdicionarNota(7.8);
+        estd.AdicionarNota(7.5);
+        estd.AdicionarNota(9.4);
         estd.CalcularMedia();
     }
 }
